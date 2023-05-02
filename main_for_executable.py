158f40fb0b3467ed5c145a91b6d145d7c30b2fe3
Thomas <67839483+Local-Coding-Buddy@users.noreply.github.com>
2023-05-01 21:20:08 -0500

    eod update

--- main_for_executable.py
+++ main_for_executable.py
@@ -17,6 +17,7 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("No environment variable SUMO_HOME!")
 
+#input parameters 
 parser = argparse.ArgumentParser(description = 'Inputs.')
 
 parser.add_argument("-r", "--random", action="store_true",
@@ -42,33 +43,31 @@ parser.add_argument("-D","--round_ID", type=str,
 args = parser.parse_args()
 #sources:
 #From https://github.com/Local-Coding-Buddy/Recursive-DUE-STR
-#
+#https://github.com/eclipse/sumo
+
+#all input hyper parameters
 network_file =args.network#'Braess_Homebrew_fixed1.net.xml'#"Braess_Homebrew_fixed1.net.xml" #'4corners_neighborhoods.net.xml'#'Random_English.net.xml'#"Braess_Homebrew_fixed1.net.xml"#"4corners_neighborhoods.net.xml" #Avenue_E_stafford_Texas.net.xml
 Round_name_base =args.round_ID#'small_test'# "On_Project"#be sure to add _Random here if you want to use the random trips 
 #(not a requirement just to keep track of which trips were generated randomly)
 num_iterations = args.Iterations
 round_min = 0 
 round_max = args.Rounds
-use_micro = True #False
-use_meso = True # TODO This might need some work aka run it on SUMO
-use_macro = True
-#use_meso_SUMO = False# run results through SUMO to ensure accuracy 
-
-#meso needs to be rerun on 3rd round of braess network.
-
-#use_macro_SUMO = False
-Number_of_vehicles = args.Number_of_vehicles
-Random_trips = args.random
-
+use_micro = True #False #run the microscopic view
+use_meso = True#run the mesoscopic view
+use_macro = True#run the macroscopic view
+Number_of_vehicles = args.Number_of_vehicles #number of vehicles when creating new trip files (rounds).
+Random_trips = args.random # use random trip assignment otherwise specifiy edges in start_edges and end_edges
 MRT = args.Release_time # Maximum Release time if not
+#for building new trip files (rounds).
 start_edges = []
 end_edges = []
 edge_dependencies = {}
-if network_file == 'Braess_Homebrew_fixed1.net.xml':
+
+if network_file == 'Braess_Homebrew_fixed1.net.xml': #by default will use these edges for braess network.
     start_edges = ["E0"]
     end_edges = ["E3"]
     edge_dependencies = {"E0":[]}
-elif network_file == '4corners_neighborhoods.net.xml':
+elif network_file == '4corners_neighborhoods.net.xml': #by default will use these edges for the UH network
     start_edges = ["597602756#0","-260998164#3","105681660#12","597602753#0","422688973","918280985","422685671#2",\
     "318908683#0"]
     end_edges = ["-597602756#0","260998164#0","-105681660#20","-597602753#1","422688976#1","918280984",\
@@ -77,10 +76,10 @@ elif network_file == '4corners_neighborhoods.net.xml':
     "597602753#0":["-597602753#1"],"422688973":["422688976#1","918280984"],"918280985":["422688976#1","918280984"],\
     "422685671#2":["15374898#0-AddedOffRampEdge","-318908683#1"],"318908683#0":["15374898#0-AddedOffRampEdge","-318908683#1"]}
 
-else:
+else:#other wise random
     Random_trips = True
 
-if Random_trips or not start_edges:
+if Random_trips or not start_edges: #for identifying rounds
     Round_name_base +="_R"
 
 
